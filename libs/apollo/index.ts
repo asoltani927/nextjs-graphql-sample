9e8ead3ec3c914299eb34deb00aa2bdb9fb6566d
Amin Soltani <asoltani927@gmail.com>
2024-02-17 17:35:38 +0330

    add error handling for graphql

--- libs/apollo/index.ts
+++ libs/apollo/index.ts
@@ -1,9 +1,27 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { ApolloClient, HttpLink, InMemoryCache, from } from "@apollo/client";
+import { onError } from "@apollo/client/link/error";
 
 const createApolloClient = () => {
+
+  const errorLink = onError(({ graphQLErrors, networkError }) => {
+    // TODO: adding Sentry for capturing errors
+    // TODO: or adding Toast message for showing errors to client and sth
+    if (graphQLErrors)
+      graphQLErrors.forEach(({ message, locations, path }) =>
+        console.log(
+          `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`
+        )
+      );
+    if (networkError) console.error(`[Network error]: ${networkError}`);
+  });
+
+  // FIXME: adding to env file
+  const httpLink = new HttpLink({ uri: 'https://swapi-graphql.netlify.app/.netlify/functions/index' })
+
   return new ApolloClient({
-    uri: "https://swapi-graphql.netlify.app/.netlify/functions/index", // FIXME: adding to env file
+    // uri: "",
     cache: new InMemoryCache(),
+    link: from([errorLink, httpLink]),
   });
 };
 
